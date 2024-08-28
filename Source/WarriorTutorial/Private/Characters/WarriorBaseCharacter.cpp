4c78543ce30d8fd5bf1c3490fd904259c443dbe0
João Fachini <joaofachini01@gmail.com>
2024-08-28 20:25:59 -0300

    Debug Helper Created

---
+++ Source/WarriorTutorial/Private/Characters/WarriorBaseCharacter.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/WarriorBaseCharacter.h"
+
+// Sets default values
+AWarriorBaseCharacter::AWarriorBaseCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	GetMesh()->bReceivesDecals = false;
+
+
+}
+
+
+
